ff57bcf0695ac74a3e043e3dad59da758a36cdf1
宇 李 <liyu@seedigitalgroup.com>
2023-04-28 17:39:26 +0800

    use cte to create snapshot

--- dbt/include/odps/macros/snapshots/snapshot.sql
+++ dbt/include/odps/macros/snapshots/snapshot.sql
@@ -84,78 +84,93 @@
 
 
 {% macro odps__snapshot_staging_table(strategy, source_sql, target_relation) -%}
-    select * from (
+    with snapshot_query as (
+        {{ source_sql }}
+    ),
+    snapshotted_data as (
+        select *,
+            {{ strategy.unique_key }} as dbt_unique_key
+        from {{ target_relation }}
+        where dbt_valid_to is null
+    ),
+    insertions_source_data as (
+        select
+            *,
+            {{ strategy.unique_key }} as dbt_unique_key,
+            cast({{ strategy.updated_at }} as timestamp) as dbt_updated_at,
+            cast({{ strategy.updated_at }} as timestamp) as dbt_valid_from,
+            cast(nullif({{ strategy.updated_at }}, {{ strategy.updated_at }}) as timestamp) as dbt_valid_to,
+            {{ strategy.scd_id }} as dbt_scd_id
+        from snapshot_query
+    ),
+    updates_source_data as (
+        select
+            *,
+            {{ strategy.unique_key }} as dbt_unique_key,
+            cast({{ strategy.updated_at }} as timestamp) as dbt_updated_at,
+            cast({{ strategy.updated_at }} as timestamp) as dbt_valid_from,
+            cast({{ strategy.updated_at }} as timestamp) as dbt_valid_to
+        from snapshot_query
+    ),
+
+    {%- if strategy.invalidate_hard_deletes %}
+    deletes_source_data as (
+        select
+            *,
+            {{ strategy.unique_key }} as dbt_unique_key
+        from snapshot_query
+    ),
+    {% endif %}
+
+    insertions as (
         select
             'insert' as dbt_change_type,
             source_data.*
-        from (
-            select
-                *,
-                {{ strategy.unique_key }} as dbt_unique_key,
-                cast({{ strategy.updated_at }} as timestamp) as dbt_updated_at,
-                cast({{ strategy.updated_at }} as timestamp) as dbt_valid_from,
-                cast(nullif({{ strategy.updated_at }}, {{ strategy.updated_at }}) as timestamp) as dbt_valid_to,
-                {{ strategy.scd_id }} as dbt_scd_id
-            from ({{ source_sql }})
-        ) as source_data
-        left outer join (
-            select *,
-                {{ strategy.unique_key }} as dbt_unique_key
-            from {{ target_relation }}
-            where dbt_valid_to is null
-        ) as snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
-        where
-            snapshotted_data.dbt_unique_key is null
-            or
-            (snapshotted_data.dbt_unique_key is not null and ({{ strategy.row_changed }}))
-    ) as insertions
-    union all
-    select * from (
+        from insertions_source_data as source_data
+        left outer join snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+        where snapshotted_data.dbt_unique_key is null
+           or (
+                snapshotted_data.dbt_unique_key is not null
+            and (
+                {{ strategy.row_changed }}
+            )
+        )
+    ),
+
+    updates as (
         select
             'update' as dbt_change_type,
             source_data.*,
             snapshotted_data.dbt_scd_id
-        from (
-            select
-                *,
-                {{ strategy.unique_key }} as dbt_unique_key,
-                cast({{ strategy.updated_at }} as timestamp) as dbt_updated_at,
-                cast({{ strategy.updated_at }} as timestamp) as dbt_valid_from,
-                cast({{ strategy.updated_at }} as timestamp) as dbt_valid_to
-            from ({{ source_sql }})
-        ) as source_data
-        join (
-            select *,
-                {{ strategy.unique_key }} as dbt_unique_key
-            from {{ target_relation }}
-            where dbt_valid_to is null
-        ) as snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
-        where ({{ strategy.row_changed }})
-    ) as updates
-    {%- if strategy.invalidate_hard_deletes %}
-    union all
-    select * from (
+        from updates_source_data as source_data
+        join snapshotted_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+        where (
+            {{ strategy.row_changed }}
+        )
+    )
+
+    {%- if strategy.invalidate_hard_deletes -%}
+    ,
+    deletes as (
         select
             'delete' as dbt_change_type,
             source_data.*,
-            {{ snapshot_get_time() }} as dbt_valid_from,
-            {{ snapshot_get_time() }} as dbt_updated_at,
-            {{ snapshot_get_time() }} as dbt_valid_to,
+            cast({{ snapshot_get_time() }} as timestamp) as dbt_valid_from,
+            cast({{ snapshot_get_time() }} as timestamp) as dbt_updated_at,
+            cast({{ snapshot_get_time() }} as timestamp) as dbt_valid_to,
             snapshotted_data.dbt_scd_id
-        from (
-            select *,
-                {{ strategy.unique_key }} as dbt_unique_key
-            from {{ target_relation }}
-            where dbt_valid_to is null
-        ) as snapshotted_data
-        left join (
-            select
-                *,
-                {{ strategy.unique_key }} as dbt_unique_key
-            from snapshot_query
-        ) as source_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
+        from snapshotted_data
+        left join deletes_source_data as source_data on snapshotted_data.dbt_unique_key = source_data.dbt_unique_key
         where source_data.dbt_unique_key is null
-    ) as deletes
+    )
+    {%- endif %}
+
+    select * from insertions
+    union all
+    select * from updates
+    {%- if strategy.invalidate_hard_deletes %}
+    union all
+    select * from deletes
     {%- endif %}
 
 {%- endmacro %}
